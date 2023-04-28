b80539135a8777120afd04febb378aa53b32988f
sssidharth <sssidharth126@gmail.com>
2023-04-28 10:25:11 +0530

    feat: API call demonstration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import ContainerComponent from './Components/Container';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=> {
+    let subscribed = true;
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      if(subscribed){
+        console.log(res.json())
+      }
+    });
+       return () => {
+        subscribed = false;
+       };
+  }, []);
   return (
     <div className="App">
      <ContainerComponent/>
